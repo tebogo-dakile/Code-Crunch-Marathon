147519301764fa951c9d2ed22f69303c5b259e4e
kyle <kkndlovu9@gmail.com>
2024-10-03 22:09:10 +0200

    dict work

---
+++ data.py
@@ -0,0 +1,10 @@
+users = {}
+#Populate the users dictionary with users names and numbers entered in the loop
+for i in range(5):
+    user_name = input("enter your name: ")
+    user_number = input("enter your phone number: ")
+
+    """TODO"""
+
+print(users)
+
--- practice.py
+++ practice.py
@@ -1,25 +1,81 @@
 # this is a practice file where you can play around with the code and test your functions or try new things
 #do not change the code in this file just edit on blank lines
 
-students = {
-    "jason": 90,
-    "james": 80,
-    "jane": 70,
-    "jill": 60,
-    "jerry": 50
-}
+# students = {
+#     "jason": 90,
+#     "james": 80,
+#     "jane": 70,
+#     "jill": 60,
+#     "jerry": 50
+# }
 
-weather = {
-    "Monday": 12,
-    "Tuesday": 14,
-    "Wednesday": 15,
-    "Thursday": 14,
-    "Friday": 21,
-    "Saturday": 22,
-    "Sunday": 24,
-}
+# weather = {
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+#     "Sunday": 24,
+# }
 
-list_nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-student_names = ["jason", "james", "jane", "jill", "jerry"]
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# student_names = ["jason", "james", "jane", "jill", "jerry"]
+
+# weather = {
+
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+#     "Sunday": 24,
+# }
+
+
+
+
+contact_book = {
+    "key":"value",
+    "Anele":607898787,
+    "Khosi":78965443221
+
+    }
+
+
+# for x in contact_book:
+#     print(x)
+
+# for value in contact_book.values():
+#     print(value)
+
+# for key,value in contact_book.items():
+#     if key == "Anele":
+#         contact_book[key] = 1
+# print(contact_book)
+
+
+# new_contacts = {"sihle":99886576,"kyle":98876554546}
+
+# contact_book.update(new_contacts)
+
+# print(contact_book)
